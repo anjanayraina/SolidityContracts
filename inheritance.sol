fc5c1123c56d64d81b61f64b70c91a7d865fb5f7
Anjanay Raina <anjanayraina326@gmail.com>
2022-09-17 11:48:06 +0530

    inheritance added

---
+++ inheritance.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+contract inheritance{
+    address owner;
+    constructor(){
+
+owner = msg.sender;
+
+
+    }
+function getOwner() public view returns(address ){
+return owner;
+
+
+}
+
+
+}
+
+contract child is inheritance{
+
+function getOwnerinChiild() public view returns (address){
+
+    return super.getOwner();
+}
+
+}
