598fca4d5c1db0c3a06ed6a0228d20d5e5dcfc76
Anjanay Raina <anjanayraina326@gmail.com>
2022-12-02 22:02:02 +0530

    Some changes in constructor

--- SharedContract.sol
+++ SharedContract.sol
@@ -4,12 +4,12 @@ pragma solidity 0.8.0;
 contract SharedWallet{
     address  payable owner;
 
-    constructor(address payable _owner){
+    constructor(address payable _owner)  payable {
         owner = _owner;
     }
     function withdrawMoney(address payable _to , uint amount )  public {
         (bool success, ) = _to.call{value : amount }("");
         require(success , "The transcation didnt go through!!");
-        
+
     }
 }
