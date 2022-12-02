06d38da73044c350e09332b0082157bb2ffe589a
Anjanay Raina <anjanayraina326@gmail.com>
2022-12-02 21:55:21 +0530

    Withdraw function added to the contratc

--- SharedContract.sol
+++ SharedContract.sol
@@ -3,8 +3,13 @@
 pragma solidity 0.8.0;
 contract SharedWallet{
     address  payable owner;
-    address payable sharedAccount;
+
     constructor(address payable _owner){
         owner = _owner;
+    }
+    function withdrawMoney(address payable _to , uint amount )  public {
+        (bool success, ) = _to.call{value : amount }("");
+        require(success , "The transcation didnt go through!!");
+        
     }
 }
