e9671378ad85bdbc1cf443837d42c03c2e67f9ec
Anjanay Raina <anjanayraina326@gmail.com>
2022-12-02 21:51:55 +0530

    Some more changes added to the Repo

--- SharedContract.sol
+++ SharedContract.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.0;
 contract SharedWallet{
     address  payable owner;
-
+    address payable sharedAccount;
     constructor(address payable _owner){
         owner = _owner;
     }
